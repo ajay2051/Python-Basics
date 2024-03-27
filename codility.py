fbbeeabd229c523ca9046c3b7072087ddcf95d06
Ajay Thakur <ajaythk.94@gmail.com>
2024-03-27 14:13:33 +0545

    >

--- codility.py
+++ codility.py
@@ -1,16 +1,3 @@
-# Solution to multiply string and integer
-def solution(s, i):
-    numbers = {"one": 1, "two": 2}
-    if s in numbers:
-        return int(numbers[s] * i)
-    else:
-        return ""
-
-
-a = solution("two", 2)
-print(a)
-
-
 class First:
     def start(self):
         print("Car Started")
